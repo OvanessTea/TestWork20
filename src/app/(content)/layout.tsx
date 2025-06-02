b0fbc7643015dc507e27b69d68a8d5dcc78ce1e8
OvanessTea <OvanessTea@yandex.com>
2025-06-02 23:40:36 +0200

    Add overflow content. Fix Footer & header

--- src/app/(content)/layout.tsx
+++ src/app/(content)/layout.tsx
@@ -1,16 +1,22 @@
 import Header from '@/components/header/Header';
 import Footer from '@/components/footer/Footer';
-
+import styles from '@/styles/main/App.module.scss';
 export default function ContentLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <>
-      <Header />
-      {children}
-      <Footer />
-    </>
+    <div className={styles.layout}>
+      <div className={styles.layout__header}>
+        <Header />
+      </div>
+      <div className={styles.layout__content}>
+        {children}
+      </div>
+      <div className={styles.layout__footer}>
+        <Footer />
+      </div>
+    </div>
   );
 }
