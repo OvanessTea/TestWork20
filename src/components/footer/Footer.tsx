4eee8688e8d31c8ddcee68b3164adacf65ba8db1
OvanessTea <OvanessTea@yandex.com>
2025-06-03 00:48:00 +0200

    add Footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,8 +1,14 @@
+import useAuthStore from '@/store/user/useAuthStore';
+import styles from '@/styles/footer/Footer.module.scss';
+
 export default function Footer() {
+    const { user } = useAuthStore((state) => state);
+
     return (
         <footer>
-            <div className='container'>
-                <p>Footer</p>
+            <div className={styles.container}>
+                <p>{new Date().getFullYear()}</p>
+                <p>{user ? `Logged as ${user.email}` : ''}</p>
             </div>
         </footer>
     );
