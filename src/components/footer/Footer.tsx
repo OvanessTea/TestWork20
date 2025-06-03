87893af495e42c1ce12cb3d522dde48e6a956063
OvanessTea <OvanessTea@yandex.com>
2025-06-03 15:56:47 +0200

    Adaptive styles for device width changing

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -7,8 +7,19 @@ export default function Footer() {
     return (
         <footer>
             <div className={styles.container}>
-                <p>2025</p>
-                <p>{user ? `Logged as ${user.email}` : ''}</p>
+                <p className={styles.year}>2025</p>
+                <p className={styles.user}>{user ? `Logged as ${user.email}` : ''}</p>
+                <div className={styles.contacts}>
+                    <div className={styles.section}>
+                        <p>+021-95-51-84</p>
+                    </div>
+                    <div className={styles.section}>
+                        <p>shop@abelohost.com</p>
+                    </div>
+                    <div className={styles.section}>
+                        <p>173 Stonecoal Road</p>
+                    </div>
+                </div>
             </div>
         </footer>
     );
